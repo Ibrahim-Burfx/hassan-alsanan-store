5f6d74cfde95807101860c21e0007fbb58447673
Ibrahim-Burfx <ibrahimibrahimmohamed645@gmail.com>
2026-09-07 07:00:30 +0300

    add google verification meta directly in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="LhQos-4qxmwnNh18SnytiJfpJGO62qDcpgxHtNzGA3c" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@500;600;700&family=Tajawal:wght@300;400;500;700&display=swap" rel="stylesheet" />
